60e1ae21856a235a96d912521f67ad8809a96314
Stuart Armstrong <Stuart@MacBook-Pro669-Armstrong.local>
2016-05-27 10:49:21 -0400

    Was working on the JSON app

---
+++ Triangle Warmup/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Triangle Warmup
+//
+//  Created by Stuart on 2016-05-27.
+//  Copyright © 2016 Stuart. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+/*
+
+*/
